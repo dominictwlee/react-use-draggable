4fb394fe5f2bfaebeaad51433830af515cc15eaa
Dominic Lee <dominictwlee@gmail.com>
2020-11-17 22:05:42 +0000

    feat: handle touch events

--- src/useDraggable.ts
+++ src/useDraggable.ts
@@ -7,12 +7,15 @@ enum DragState {
   DRAGGING = 'dragging',
 }
 
+type EventType = 'mouse' | 'touch';
+
 export default function useDraggable<T extends HTMLElement>() {
   const ref = useRef<T>(null);
   const [translateX, setTranslateX] = useState(0);
   const [translateY, setTranslateY] = useState(0);
   const lastTranslateX = useRef(0);
   const lastTranslateY = useRef(0);
+  const eventType = useRef<EventType>('mouse');
 
   const [lastX, setLastX] = useState<number | null>(null);
   const [lastY, setLastY] = useState<number | null>(null);
@@ -36,15 +39,33 @@ export default function useDraggable<T extends HTMLElement>() {
   );
 
   const handleDrag = useCallback(
+    (clientX: number, clientY: number) => {
+      const { deltaX, deltaY } = calcDragDelta(clientX, clientY);
+      setTranslateX(lastTranslateX.current + deltaX);
+      setTranslateY(lastTranslateY.current + deltaY);
+    },
+    [calcDragDelta]
+  );
+
+  const handleMouseDrag = useCallback(
     (e: MouseEvent) => {
       if (dragState !== DragState.DRAGGING) {
         return;
       }
-      const { deltaX, deltaY } = calcDragDelta(e.clientX, e.clientY);
-      setTranslateX(lastTranslateX.current + deltaX);
-      setTranslateY(lastTranslateY.current + deltaY);
+      handleDrag(e.clientX, e.clientY);
     },
-    [calcDragDelta, dragState]
+    [dragState, handleDrag]
+  );
+
+  const handleTouchDrag = useCallback(
+    (e: TouchEvent) => {
+      if (dragState !== DragState.DRAGGING) {
+        return;
+      }
+      const touch = e.touches[0];
+      handleDrag(touch.clientX, touch.clientY);
+    },
+    [dragState, handleDrag]
   );
 
   const handleDragEnd = useCallback(() => {
@@ -59,36 +80,70 @@ export default function useDraggable<T extends HTMLElement>() {
   useEffect(() => {
     const node = ref.current;
     if (dragState === DragState.DRAGGING) {
-      node?.ownerDocument.addEventListener('mousemove', handleDrag);
+      switch (eventType.current) {
+        case 'mouse':
+          node?.ownerDocument.addEventListener('mousemove', handleMouseDrag);
+          break;
+        case 'touch':
+          node?.ownerDocument.addEventListener('touchmove', handleTouchDrag);
+          break;
+        default:
+          break;
+      }
     }
     return () => {
-      node?.ownerDocument.removeEventListener('mousemove', handleDrag);
+      node?.ownerDocument.removeEventListener('mousemove', handleMouseDrag);
+      node?.ownerDocument.removeEventListener('touchmove', handleTouchDrag);
     };
-  }, [dragState, handleDrag]);
+  }, [dragState, handleDrag, handleMouseDrag, handleTouchDrag]);
 
-  const handleDragStart = useCallback((e: React.MouseEvent) => {
-    const node = ref.current;
-    if (e.button !== MOUSE_LEFT_BUTTON || !node) {
-      return;
-    }
-    const offsetParent = node.offsetParent || node.ownerDocument.body;
+  const handleDragStart = useCallback(
+    (clientX: number, clientY: number, node: T) => {
+      const offsetParent = node.offsetParent || node.ownerDocument.body;
 
-    const isBody = offsetParent === offsetParent.ownerDocument.body;
-    const offsetParentRect = isBody
-      ? { left: 0, top: 0 }
-      : offsetParent.getBoundingClientRect();
+      const isBody = offsetParent === offsetParent.ownerDocument.body;
+      const offsetParentRect = isBody
+        ? { left: 0, top: 0 }
+        : offsetParent.getBoundingClientRect();
 
-    setDragState(DragState.DRAGGING);
-    setLastX(e.clientX + offsetParent.scrollLeft - offsetParentRect.left);
-    setLastY(e.clientY + offsetParent.scrollTop - offsetParentRect.top);
-  }, []);
+      setDragState(DragState.DRAGGING);
+      setLastX(clientX + offsetParent.scrollLeft - offsetParentRect.left);
+      setLastY(clientY + offsetParent.scrollTop - offsetParentRect.top);
+    },
+    []
+  );
 
-  console.log({ translateX, translateY, lastX, lastY });
+  const onMouseDown = useCallback(
+    (e: React.MouseEvent) => {
+      const node = ref.current;
+      if (e.button !== MOUSE_LEFT_BUTTON || !node) {
+        return;
+      }
+      eventType.current = 'mouse';
+      handleDragStart(e.clientX, e.clientY, node);
+    },
+    [handleDragStart]
+  );
+
+  const onTouchStart = useCallback(
+    (e: React.TouchEvent) => {
+      const node = ref.current;
+      if (!node) {
+        return;
+      }
+      eventType.current = 'touch';
+      const touch = e.touches[0];
+      handleDragStart(touch.clientX, touch.clientY, node);
+    },
+    [handleDragStart]
+  );
 
   return {
     ref,
-    onMouseDown: handleDragStart,
+    onMouseDown,
+    onTouchStart,
     onMouseUp: handleDragEnd,
+    onTouchEnd: handleDragEnd,
     style: { transform: `translate(${translateX}px, ${translateY}px)` },
   };
 }
