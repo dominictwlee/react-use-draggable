68c9f7c5f3b0a1ee39e853a22d07b6281df65f2f
Dominic Lee <dominictwlee@gmail.com>
2020-11-19 13:08:23 +0000

    chore: move mock component to stories

--- stories/Thing.stories.tsx
+++ stories/Thing.stories.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { FC, HTMLAttributes, ReactChild } from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { useDraggable } from '../src';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
+  children?: ReactChild;
+}
+
+const Thing: FC<Props> = ({ children }) => {
+  const { style, ...draggableProps } = useDraggable<HTMLDivElement>();
+  return (
+    <div
+      {...draggableProps}
+      style={{
+        height: 100,
+        width: 100,
+        backgroundColor: 'yellow',
+        ...style,
+      }}
+    >
+      {children || `the snozzberries taste like snozzberries`}
+    </div>
+  );
+};
 
 const meta: Meta = {
   title: 'Welcome',
